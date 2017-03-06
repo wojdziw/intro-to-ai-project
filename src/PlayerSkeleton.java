16e2002983184ebeda6ec851ac207c302820989e
wojdziw <wojciech.dziwulski@gmail.com>
2017-03-06 18:13:45 +0800

    Added the static keyword

--- src/PlayerSkeleton.java
+++ src/PlayerSkeleton.java
@@ -7,29 +7,29 @@ public class PlayerSkeleton {
 		return 0;
 	}
 
-	public int calculateFeature1(State s) {
+	public static int calculateFeature1(State s) {
 		int featureValue = 0;
 		return featureValue;
 	}
 
-	public int calculateFeature2(State s) {
+	public static int calculateFeature2(State s) {
 		int featureValue = 0;
 		return featureValue;
 	}
 
-	public int calculateFeature3(State s) {
+	public static int calculateFeature3(State s) {
 		int featureValue = 0;
 		return featureValue;
 	}
 
-	public int calculateUtility(State s) {
+	public static int calculateUtility(State s) {
 		int noFeatures = 3;
 
 		int[] weights = new int[noFeatures];
 
 		int feature1 = calculateFeature1(s);
 		int feature2 = calculateFeature2(s);
-		int feature3 = calculateFeature2(s);
+		int feature3 = calculateFeature3(s);
 
 		int utility = weights[0]*feature1 + weights[1]*feature2 + weights[2]*feature3;
 
@@ -49,6 +49,8 @@ public class PlayerSkeleton {
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
+
+			System.out.println(calculateUtility(s));
 		}
 		System.out.println("You have completed "+s.getRowsCleared()+" rows.");
 	}
