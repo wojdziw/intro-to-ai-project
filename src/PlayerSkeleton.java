10f6a5a6b77c3f93d86dcc20dbf4df22e7223387
wojdziw <wojciech.dziwulski@gmail.com>
2017-03-06 18:01:29 +0800

    Sample features

--- src/PlayerSkeleton.java
+++ src/PlayerSkeleton.java
@@ -3,8 +3,37 @@ public class PlayerSkeleton {
 
 	//implement this function to have a working system
 	public int pickMove(State s, int[][] legalMoves) {
-		
+
 		return 0;
+	}
+
+	public int calculateFeature1(State s) {
+		int featureValue = 0;
+		return featureValue;
+	}
+
+	public int calculateFeature2(State s) {
+		int featureValue = 0;
+		return featureValue;
+	}
+
+	public int calculateFeature3(State s) {
+		int featureValue = 0;
+		return featureValue;
+	}
+
+	public int calculateUtility(State s) {
+		int noFeatures = 3;
+
+		int[] weights = new int[noFeatures];
+
+		int feature1 = calculateFeature1(s);
+		int feature2 = calculateFeature2(s);
+		int feature3 = calculateFeature2(s);
+
+		int utility = weights[0]*feature1 + weights[1]*feature2 + weights[2]*feature3;
+
+		return utility;
 	}
 	
 	public static void main(String[] args) {
