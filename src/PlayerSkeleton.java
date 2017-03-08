eff7d451787127bbb6e45182dbe05b21b4df929c
qufeichen <qufei.chen@gmail.com>
2017-03-08 15:54:05 +0800

    tested and cleaned up code

--- src/PlayerSkeleton.java
+++ src/PlayerSkeleton.java
@@ -10,12 +10,14 @@ public class PlayerSkeleton {
 	
 	//FEATURE 5
 	//Height differences - sum of the height differences between adjacent columns
+	/*
+	 * TODO: can we remove this feature and just keep feature 15 instead?
+	 */
 	public static double calculateFeature5(State s) {
 		
 		double sumHeightDiff = 0;
 		
-		//top[] holds the height of each column
-		int[] top = s.getTop();
+		int[] top = s.getTop(); //holds the height of each column
 		
 		for (int i = 1; i<top.length; i++) {
 			sumHeightDiff += top[i-1] - top[i];
@@ -27,10 +29,9 @@ public class PlayerSkeleton {
 	//FEATURE 10
 	//Well Count - The number of uncovered holes that are 3 or more blocks deep
 	public static int calculateFeature10(State s) {
-		int numberOfWells = 0;
 		
-		//top[] holds the height of each column
-		int[] top = s.getTop();
+		int numberOfWells = 0;
+		int[] top = s.getTop(); //holds the height of each column
 		
 		for (int i = 1; i<top.length; i++) {
 			int diff = Math.abs(top[i-1] - top[i]);
@@ -39,16 +40,19 @@ public class PlayerSkeleton {
 			}
 		}
 		
+		System.out.println("Number of wells: "+numberOfWells);
 		return numberOfWells;
 	}
 	
 	//FEATURE 15
 	//Column difference - height difference between each pair of adjacent columns
-	//TODO: do we really need both sum height differences and individual column differences?
-	//Instead of returning 15 distinct values, I have applied the individual column weights directly in this function, and opted to return the sum
-	//i.e. This function will return the weighted sum of column weights (instead of 9 unique column height differences)
-	//This way we can get rid of feature 5, and instead just have this weighted version of column heights instead
-	//An additional weight for the sum of column heights can still be added in calculateUtility()
+	/*
+	 * TODO: do we really need both sum height differences and individual column differences?
+	 * Instead of returning 15 distinct values, I have applied the individual column weights directly in this function, and opted to return the sum
+	 * i.e. This function will return the weighted sum of column weights (instead of 9 unique column height differences)
+	 * This way we can get rid of feature 5, and instead just have this weighted version of column heights instead
+	 * An additional weight for the sum of column heights can still be added in calculateUtility()
+	 */
 	public static double calculateFeature15(State s, double[] colDiffWeights) {
 		
 		double weightedSumHeightDiff = 0;
@@ -67,7 +71,6 @@ public class PlayerSkeleton {
 	//implement this function to have a working system
 	public int pickMove(State s, int[][] legalMoves) {
 		
-		testPrintBoard(s);
 		return 0;
 		
 	}
@@ -77,16 +80,17 @@ public class PlayerSkeleton {
 
 		double[] weights = new double[noFeatures];
 		
-		//TODO: set weights
-		//TODO: maybe start our naming for features from 0 instead of 1? 
-		// - that will make the naming of our weights more consistent
-		weights[4] = 0.0;
-		weights[9] = 0.0;
-		weights[14] = 0.0;
+		//Set weights
+		/*
+		 * TODO: maybe start our naming for features from 0 instead of 1? 
+		 * - that will make the naming of our weights more consistent
+		 */
+		weights[4] = 0.4;
+		weights[9] = 0.2;
+		weights[14] = 0.6;
 		
-		//Column difference weights
+		//Column Difference weights
 		//Used for feature 15 when calculating importance of individual column-pairs (otherwise why split calculate individual pairings?)
-		//TODO: set weights for individual pairs of columns
 		double[] colDiffWeights = new double[s.COLS-1];
 		colDiffWeights[0] = 0.0;
 		colDiffWeights[1] = 0.0;
@@ -105,7 +109,7 @@ public class PlayerSkeleton {
 
 		//apply weights
 		double utility = weights[4]*feature5 + weights[9]*feature10 + weights[14]*feature15;
-
+		
 		return utility;
 	}
 	
@@ -143,12 +147,10 @@ public class PlayerSkeleton {
 		
 		System.out.println("Printing field:");
 		for (int i = 0; i<field.length; i++) {
-			
 			for(int j = 0; j<field[i].length; j++) {
 				System.out.print(field[i][j] + " ");
 			}
 			System.out.println();
-			
 		}
 		System.out.println();
 		
