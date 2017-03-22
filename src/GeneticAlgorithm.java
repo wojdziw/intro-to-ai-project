0b63e86ab265de0be3dc04bf99b1c8fda85df1e8
wojdziw <wojciech.dziwulski@gmail.com>
2017-03-22 12:05:50 +0800

    Minor changes

--- src/GeneticAlgorithm.java
+++ src/GeneticAlgorithm.java
@@ -31,7 +31,7 @@ public class GeneticAlgorithm {
     private static final int noFeatures = 15 + (State.COLS - 1) + (State.COLS -2); //noFeatures + columnHeightWeights + columnDifferenceWeights
     private static final double maxWeight = 3;
     private static final int populationSize = 50;
-    private static final int noGenerations = 40;
+    private static final int noGenerations = 50;
 
     private static final double uniformRate = 0.5;
     private static final double mutationRate = 0.015;
